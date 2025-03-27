59f0983a6a157efddf9036f2db4328b5893c8fef
J-Umanzor <umanzorjeffrey628@gmail.com>
2025-03-27 00:52:20 -0400

    remove pages folder

--- frontend/src/pages/polygontest.js
+++
@@ -1,8 +0,0 @@
-import Login from '@/components/Login';
-import Polygon from '@/components/PolygonAPITest';
-
-const PolygonTestPage = () => {
-  return <Polygon />;
-};
-
-export default PolygonTestPage;
