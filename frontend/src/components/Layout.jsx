cee31099676ff6700bcdddab5839c48f43475350
MartinLahoumh <martinlahoumh103102@gmail.com>
2025-03-30 17:07:32 -0400

    added user context so user can be global

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,40 +1,14 @@
 import Navbar from '../components/Navbar';  
 import Footer from '../components/Footer';
-import {supabase} from '@/config/supabaseClient';
-import { use, useEffect, useState } from 'react';
+import { GlobalUser } from '@/config/UserContext';
 
 const Layout = ({children}) =>{
-    //This is where we will store the user session
-const [user, setUser] = useState(null);
-
-useEffect(() =>{
-    const fetchSession = async () => {
-    const { data, error } = await supabase.auth.getSession();
-    if (error) {
-        console.error('Error fetching session:', error.message);
-        return;
-    }
-    if (data?.session?.user) {
-        setUser(data.session.user);
-    }
-    };
-
-    fetchSession();
-
-    const { data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
-    setUser(session?.user || null)});
-
-    return () => {
-    authListener?.subscription.unsubscribe();
-    };
-}, []);
-
     return(
-        <div>
-            <Navbar user={user}/>
+        <GlobalUser>
+            <Navbar/>
             {children}
             <Footer/>
-        </div>
+        </GlobalUser>
     )
 }
 export default Layout;
---
+++ frontend/src/config/UserContext.js
@@ -0,0 +1,35 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { supabase } from "@/config/supabaseClient";
+
+const UserContext = createContext(null);
+
+export const GlobalUser = ({children}) =>{
+//This is where we will store the user session
+const [user, setUser] = useState(null);
+
+useEffect(() =>{
+    const fetchSession = async () => {
+    const { data, error } = await supabase.auth.getSession();
+    if (error) {
+        console.error('Error fetching session:', error.message);
+        return;
+    }
+    if (data?.session?.user) {
+        setUser(data.session.user);
+    }
+    };
+
+    fetchSession();
+
+    const { data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
+    setUser(session?.user || null)});
+
+    return () => {
+    authListener?.subscription.unsubscribe();
+    };
+}, []);
+
+return <UserContext.Provider value={user}>{children}</UserContext.Provider>
+};
+
+export const globalUser = () => useContext(UserContext);
